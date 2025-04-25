2b63cdb8f083090bdefa8d697bc9bc5a19746f59
YRL-M <yrl.maillard@gmail.com>
2025-04-25 10:50:39 +0200

    Update arithmetique.py

--- arithmetique.py
+++ arithmetique.py
@@ -10,50 +10,50 @@
 
 #   ------------------   #
 
-def heron(n:int, verbose=False) :
+def heron(n:int, verbose=False):
     """
     Effectue l'algorithme de Héron, avec des divisions ENTIERES
-    Renvoie b, r : booléen quadraticité de n et la racine entière de n.
+    Renvoie b, r : booléen de quadraticité de n et la racine entière de n.
     """
 
-    if n == 0 :
-        return True, 0
-    if not isinstance(n, int):
-        raise TypeError(f"{n} doit être un int")
-    if n < 0 :
-        raise ValueError(f"{n} doit être positif")
-
+    if n == 0:                 return True, 0
+    if not isinstance(n, int): raise TypeError(f"{n} doit être un entier")
+    if n < 0 :                 raise ValueError(f"{n} doit être positif")
+  
+    # Initialisation 
     (L, l) = (n, 1)
 
+    # Boucle principale
     for _ in range( 100000 ) :
-        L = quotient(L + l, 2)
-        l = quotient(n, L)
-        if verbose :
-            print(L, l)
+        L = quotient(L+l, 2)   # moyenne ENTIÈRE
+        l = quotient(n, L)     # conservation de matière 
+        if verbose: print(L, l)
+      
         if L*L == n:
             return True, L
-        if L <= l :
+        if L <= l:
             return False, L
     else: # for,else:
         raise RuntimeError(f"Attention, heron({n}) a effetué 100000 itérations, c'est anormal.")
 
  
 
-def quadraticité(n):
+def quadratic(n):
     """
-    Renvoie le booléen si True ou False n est un carré parfait.
-    Basé sur b,r = heron(n)
-    quadraticité(n) >> b True ou False
+    Renvoie le booléen True ou False si n est un carré parfait.
+    Basé sur b, r = heron(n)
+    quadratic(n) >> b
     """
     b, r = heron(n)
     return b
   
 def iracine(n):
     """
-    Renvoie la racine entière exacte sur n est un carré parfait.
-    Ou approximative si n n'en est pas un.
+    Renvoie la racine entière de n
+    Résultat exact si n est un carré parfait.
+    Résultat approximatif sinon.
     Basé sur b,r = heron(n)
-    iracine(n) >> r (int)
+    iracine(n) >> r 
     """
     b, r = heron(n)
     return r
@@ -65,16 +65,12 @@ def division(D:int, d:int):
     division(D, d) >> q, r
     """
 
-    if d==0:
-        raise ZeroDivisionError(f"Dividende = {D}, diviseur = {d}")
-    if type(D)!=int :
-        raise TypeError(f"{D} n'est pas un entier.")
-    if type(d)!=int:
-        raise TypeError(f"{d} n'est pas un entier.")
-    if d < 0:
-        raise ValueError(f"Le diviseur {d} doit être positif.")
-    if D < 0:
-        raise ValueError(f"Le Dividende {D} doit être positif.")
+    if type(D)!=int: raise TypeError(f"{D} n'est pas un entier.")
+    if type(d)!=int: raise TypeError(f"{d} n'est pas un entier.")
+    if d==0:         raise ZeroDivisionError(f"Dividende = {D}, diviseur = {d}")
+    if D==0:         return 0, 0
+    if d < 0:        raise ValueError(f"Le diviseur {d} doit être positif.")
+    if D < 0:        raise ValueError(f"Le Dividende {D} doit être positif.")
 
     # Initialisation
     q, r = 0, D
